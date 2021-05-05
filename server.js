b8fff34f1bce8acf01067408e938462f53761a4e
Your Name <you@example.com>
2021-05-05 11:38:36 -0400

    POST

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express= require('express');
 const PORT= process.env.PORT || 3001;
+const fs= require('fs');
+const { clear } = require('node:console');
+const { type } = require('node:os');
+const path= require('path');
+const { setFlagsFromString } = require('v8');
 
 const app= express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const { animals }= require('./data/animals.json');
 
@@ -40,6 +48,33 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal= body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results= animals;
     if (req.query) {
@@ -51,7 +86,20 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req, res) => {
     const result= findById(req.params.id, animals);
     res.json(result);
-})
+});
+
+app.post('/api/animals', (req, res) => {
+    // console.log(req.body);
+    req.body.id= animals.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        const animal= createNewAnimal(req.body, animals);
+        res.json(animal);
+    }  
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
